82a7e19413b81d19017c29e41ecf880ebba8c042
Trevor Capozza <trevorcapozza@Trevors-MacBook-Pro.local>
2022-10-03 15:10:34 -0700

    footer

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,19 @@
       <router-link to="/about">About</router-link>
     </nav> -->
     <router-view/>
+    <FooterView/>
   </div>
 </template>
+
+<script>
+import FooterView from '@/components/FooterView'
+
+export default {
+  components: {
+    FooterView,
+  },
+}
+
+</script>
 
 <style src="@/css/main.scss" lang="scss" />
--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="footer"><p>Copyright © {{ new Date().getFullYear() }} Department of Private Affairs</p></div>
+    <div class="footer"><p>COPYRIGHT © {{ new Date().getFullYear() }} DEPARTMENT OF PRIVATE AFFAIRS</p></div>
   </template>
   
   <script>
   export default {
-    
+    name: 'FooterView',
   }
   </script>
