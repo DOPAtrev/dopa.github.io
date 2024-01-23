e61965dee844a5d6ce1865e712145d75dad7939a
trevorcapozza <trevor.capozza@gmail.com>
2024-01-22 16:57:36 -0800

    bg stuff

--- src/components/VantaBg.vue
+++ src/components/VantaBg.vue
@@ -1,23 +1,8 @@
-<template onselectstart="return false">
-<div class="background" ref="vantaBg">
-
-  <div class="navbar max">
-      <img alt="Dopa logo" class="logo" src="../assets/round-logo-web.svg">
-  </div>
-
-  <div class="home">
-    <div class="home-content max">
-      <!-- <img alt="blackhawk" class="blackhawk" src="../assets/blackhawk.svg"> -->
-      <h2>Department of<br>Private Affairs.</h2>
-    </div>
-  </div>
-
-</div>
-</template>
+<template>
+    <div ref="vantaBg"></div>
+</template>  
 
 <script>
-document.oncontextmenu =new Function("return false;")
-
 import * as THREE from 'three';
 import GLOBE from 'vanta/dist/vanta.globe.min';
 
@@ -42,5 +27,4 @@ export default {
     if (this.vantaEffect) this.vantaEffect.destroy();
   }
 };
-
 </script>
